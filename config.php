0c375035b2213e4534b7627100fda9dd8c4b0f8c
ikhsannugs <59926991+ikhsannugs@users.noreply.github.com>
2020-03-10 22:43:58 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
-$db_host = "localhost";
-$db_user = "devopscilsy";
-$db_pass = "1234567890";
-$db_name = "dbsosmed";
+$db_host = getenv("DB_HOST");
+$db_user = getenv("DB_USER");
+$db_pass = getenv("DB_PASS");
+$db_name = getenv("DB_NAME");
+
 
 try {    
     //create PDO connection 
